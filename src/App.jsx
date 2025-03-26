6c5930a97cc6b3ed1cc2e48dd0683bc2596a4793
Charu Kush <charu.kush@gmail.com>
2025-03-26 12:50:25 -0600

    Props concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import reactImage from './assets/react-core-concepts.png';
+import reactImage from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -20,11 +21,42 @@ function Header() {
   );
 }
 
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts
+              {...CORE_CONCEPTS[1]}
+            />
+            <CoreConcepts
+              {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
         <h2>Time to get started with ReactEssential!</h2>
       </main>
     </div>
