531ea6aca5c48140f10deabc7cd82c9093a96e12
Charu Kush <charu.kush@gmail.com>
2025-03-29 19:37:38 -0600

    Add example data and update tab selection to display example details

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,28 @@ import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts/CoreConcepts";
 import TabButton from "./components/TabButton";
+import { EXAMPLES } from "./data"
 
 function App() {
-  const [selectedTab, setSelectedTab] = useState();
+  const [selectedTab, setSelectedTab] = useState('components');
 
   function handleSelect(selectHandler) {
     setSelectedTab(selectHandler);
+  }
+  
+  let tabSelector = <p>Please select the tab</p>;
+  if (selectedTab) {
+    tabSelector = (
+      <div>
+        <h3>{EXAMPLES[selectedTab].title}</h3>
+        <p>{EXAMPLES[selectedTab].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTab].code}
+          </code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -35,15 +51,15 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("Component")}>
+            <TabButton onSelect={() => handleSelect("components")}>
               Component
             </TabButton>
-            <TabButton onSelect={() => handleSelect("JSXgjghjghj")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
         </section>
-        <p>{selectedTab}</p>
+        {tabSelector}
       </main>
     </div>
   );
